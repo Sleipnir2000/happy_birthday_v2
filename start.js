d68f3adad3403af791ebc3b2059d54cfc275430b
Sleipnir2000 <jionghua_chen@ucsb.edu>
2022-12-09 18:57:35 -0800

    Done

--- start.js
+++ start.js
@@ -1,17 +1,53 @@
+/*
+ * Represent a discrete hour glass which triggers some specified action after being activated some specified number of times. 
+ */
+class HourGlass {
+    constructor(times, action) {
+     if (!Number.isInteger(times) || times < 1) {
+         throw "times must be an integer greater than 0";
+     } 
+     if (!(action instanceof Function)) {
+         throw "action must be a function";
+     }
+     this.times = times;
+     this.action = action;
+    }
+ 
+    activate() {
+     this.times -= 1;
+     if (this.times === 0) {
+         this.action();
+     }
+    }
+ }
 
+// instructions begin
 const isCandlesOn = new Array(6).fill(false);
 const COLOR_LIGHTING_CANDLE = "#f79347";
+const COLOR_UNLIGHT_CANDLE = "#c0c0c0";
+const ACTION_LIMIT = 10;
+const hourGlass = new HourGlass(ACTION_LIMIT, () => {
+    alert("💥💥💥💥💥💥💥💥💥💥💥💥💥💥")
+    window.location.href = "https://wwwwwwwww.jodi.org/100cc/hqx/i900.html"
+});
+alert("谜题：点燃所有🕯️");
 
 function lightCandle(index) {
     const candle = document.getElementById(`candle_${index}`);
     candle.style.color = COLOR_LIGHTING_CANDLE;
+    for (let flame of document.getElementsByClassName(`flame_${index}`)) {
+        flame.style.color = COLOR_LIGHTING_CANDLE;
+    }
     isCandlesOn[index] = true;
     console.log(isCandlesOn);
 }
 
 function extinguishCandle(index) {
     const candle = document.getElementById(`candle_${index}`);
-    candle.style.color = "black";
+    candle.style.color = COLOR_UNLIGHT_CANDLE;
+    for (let flame of document.getElementsByClassName(`flame_${index}`)) {
+        flame.style.color = COLOR_UNLIGHT_CANDLE;
+    }
     isCandlesOn[index] = false;
     console.log(isCandlesOn);
 }
@@ -39,6 +75,7 @@ function touchCandle(event) {
     if (isCandlesOn.reduce((acc,val) => acc && val)) {
         alert("All candles on!");
     }
+    hourGlass.activate();
 }
 
 // Add click listener for candles
